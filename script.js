303f2c8045920ff67c9efb9f22b239e0f0f3aeb6
KozlovAleksandr <aleksandr.an.kozlov@gmail.com>
2021-11-09 12:47:57 +0300

    checking is added

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = prompt('На сколько его оцените?', ''),
-  c = prompt('Один из последних просмотренных фиьмов?', ''),
-  d = prompt('На сколько его оцените?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  // if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+  //   personalMovieDB.movies[a] = b;
+  // } else {
+  //   i--;
+  // }
+
+  (a != null && b != null && a != '' && b != '' && a.length < 50) ?
+  personalMovieDB.movies[a] = b: i--;
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Очень мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Нормально фильмов");
+} else if (personalMovieDB.count > 30) {
+  console.log("Много фильмов");
+} else {
+  console.log("Ошибка");
+}
 
 console.log(personalMovieDB);
